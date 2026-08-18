2f35c2cc9dd98784c32af04172ae1eff8ead0fc5
Sriram-Nambiar <sriramnambiar21@gmail.com>
2026-08-18 14:39:53 +0530

    Add Header and LeaderboardTable components; update Home page layout

---
+++ components/Header.tsx
@@ -0,0 +1,14 @@
+export default function Header() {
+    return (
+        <div className="border border-white p-4 text-lg h-15 flex justify-between items-center">
+            <div>
+                DevBench
+            </div>
+            <div className="flex gap-6">
+                <h4>login</h4>
+                <h4>login</h4>
+                <h4>login</h4>
+            </div>
+        </div>
+    )
+}
