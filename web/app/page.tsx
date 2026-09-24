a86c68f9f875ce764a929720efc10681f2b98243
Sriram-Nambiar <sriramnambiar21@gmail.com>
2026-09-24 21:20:34 +0530

    consolidate web into root repository

---
+++ web/app/page.tsx
@@ -0,0 +1,17 @@
+
+import Header from "@/components/Header"
+import LeaderboardTable from "@/components/LeaderboardTable"
+export default function Home() {
+    return (
+        <>
+
+        <div className="p-0">
+           <Header/>
+        </div>
+        <div className="mx-3 my-8 w-100 p-2 text-4xl font-extrabold">
+            A challenging, contamination-free LLM benchmark.
+        </div>
+        <LeaderboardTable/>
+        </>
+    )
+}
